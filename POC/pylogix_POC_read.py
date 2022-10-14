3674f31556b10e21c119900093e5f3682eb00b15
bernauers <44301851+bernauers@users.noreply.github.com>
2022-10-14 07:52:51 -0700

    Add pylogix_POC_read.py

---
+++ POC/pylogix_POC_read.py
@@ -0,0 +1,7 @@
+from pylogix import PLC
+with PLC() as comm:
+    comm.IPAddress = 'IP_Address'
+    ret = comm.Read('tag_name')
+    print("tagname:"+ str(ret.TagName)+'\n', "value:"+ str(ret.Value)+'\n', "status:"+ str(ret.Status)+'\n')
+    
+    comm.Close()
